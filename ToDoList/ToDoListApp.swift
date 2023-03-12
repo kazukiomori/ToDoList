d8e3eb6b6210e334a9c6e64af514d0f68473143d
kazukiomori <kazuki1110170501@gmail.com>
2023-03-13 00:01:37 +0900

    アプリ初回起動時にATTのダイアログを表示する

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -15,27 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication,
                       didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
                         GADMobileAds.sharedInstance().start(completionHandler: nil)
-                        requestAppTrackingTransparencyAuthorization()
                         return true
                     }
-    
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        
-    }
-    
-    private func requestAppTrackingTransparencyAuthorization() {
-        if #available(iOS 14.5, *) {
-            // .notDeterminedの場合にだけリクエスト呼び出しを行う
-            guard ATTrackingManager.trackingAuthorizationStatus == .notDetermined else { return }
-            
-            // タイミングを遅らせる為に処理を遅延させる
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-                    // リクエスト後の状態に応じた処理を行う
-                })
-            }
-        }
-    }
 }
 
 @main
@@ -45,6 +26,9 @@ struct ToDoListApp: App {
         WindowGroup {
             TodoList(todoList: .constant(TodoModel().taskList),selectedTab: TaskStatus.new )
                 .environmentObject(todoModel)
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in })
+                        }
         }
     }
 }
